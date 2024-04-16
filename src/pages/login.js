9640cf994d896b46467a02dc02142e56231f8bf4
Prakhar Jain <prakhar.jain@qure.ai>
2024-04-17 02:38:50 +0530

    issue fix

--- src/pages/login.js
+++ src/pages/login.js
@@ -1,29 +1,27 @@
 import { createClient } from "@supabase/supabase-js";
 import { useEffect, useState } from "react";
-import { Auth } from '@supabase/auth-ui-react';
-import { ThemeSupa } from '@supabase/auth-ui-shared';
-import { useNavigate } from 'react-router-dom';
-import { createContext } from 'react';
+import { Auth } from "@supabase/auth-ui-react";
+import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { useNavigate } from "react-router-dom";
+import { createContext } from "react";
 import MenuBoard from "./menuBoard";
 
+const supabaseUrl = process.env.REACT_APP_SUPABASE_URL;
+const supabaseAnonKey = process.env.REACT_APP_SUPABASE_ANON_KEY;
 
-const supabase = createClient(
-  "https://simggheqbtucnadaqtsh.supabase.co", 
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNpbWdnaGVxYnR1Y25hZGFxdHNoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTMwMzczMTQsImV4cCI6MjAyODYxMzMxNH0.hNu8JoYlIXR1frZY8KJwFmoaETL_opFgKqs6TvVvtLE"
-);
+const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export const AuthContext = createContext(null);
 
 function Login() {
-
-  const [session, setSession] = useState(null)
-  const [isLoggedIn, setIsLoggedIn] = useState(false)
+  const [session, setSession] = useState(null);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
   const navigate = useNavigate();
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }) => {
-      setSession(session)
-      setIsLoggedIn(true)
+      setSession(session);
+      setIsLoggedIn(true);
       // console.log('found existing session')
       // console.log(session)
       // console.log(session.user.email)
@@ -32,37 +30,39 @@ function Login() {
     const {
       data: { subscription },
     } = supabase.auth.onAuthStateChange((_event, session) => {
-      setSession(session)
-      setIsLoggedIn(true)
+      setSession(session);
+      setIsLoggedIn(true);
       // console.log('event listener found a new session')
       // console.log(session)
       // console.log(session.user.email)
     });
 
-    return () => subscription.unsubscribe()
-  }, [])
+    return () => subscription.unsubscribe();
+  }, []);
 
   if (!session) {
-    return (<Auth 
-      supabaseClient={supabase}
-      appearance={{ theme: ThemeSupa }} 
-      providers={["google"]}
-      />)
-  }
-  else {
     return (
-    <>
-    <AuthContext.Provider value={{session, setSession, supabase, isLoggedIn, setIsLoggedIn}}>
-      {/* <div>{session.user.email.split('@')[0]} you are logged in!</div>
+      <Auth
+        supabaseClient={supabase}
+        appearance={{ theme: ThemeSupa }}
+        providers={["google"]}
+      />
+    );
+  } else {
+    return (
+      <>
+        <AuthContext.Provider
+          value={{ session, setSession, supabase, isLoggedIn, setIsLoggedIn }}
+        >
+          {/* <div>{session.user.email.split('@')[0]} you are logged in!</div>
         <button className="go-to-menu" onClick={() => navigate('/menu')} >
           Go to Menu
         </button> */}
-        <MenuBoard />
-    </AuthContext.Provider>
-    </>
-  )
-  };
-
+          <MenuBoard />
+        </AuthContext.Provider>
+      </>
+    );
+  }
 }
 
 export default Login;
