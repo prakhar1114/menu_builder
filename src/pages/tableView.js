3a02523f27db81937beb2615065606103b803279
Prakhar Jain <prakhar.jain@qure.ai>
2024-05-29 10:53:53 +0530

    selection corrected and switching to tex tarea

--- src/pages/tableView.js
+++ src/pages/tableView.js
@@ -7,7 +7,38 @@ import "../tableView.css";
 
 function TableView() {
 
-  const [userMenu, setTabularData] = useState(tabularData);
+  const [isFocused, setIsFocused] = useState(false);
+  const [userMenu, setUserMenu] = useState(tabularData);
+
+  const defaultMealState = tabularData.map(
+    (val) => ({
+      Day: val.Day,
+      Breakfast: false,
+      Lunch: false,
+      Dinner: false,
+    })
+  );
+
+  const [mealSelectedArray, setMealSelected] = useState(defaultMealState);
+
+  const toggleMealSelected = (index, field, value) => {
+    console.log("Toggling others off")
+
+    const toggleOffEverything = [...defaultMealState];
+
+    toggleOffEverything[index][field] = value;
+    setMealSelected(toggleOffEverything);
+  };
+
+    // Function to handle changes in input fields
+  const handleChange = (index, field, value) => {
+    console.log("Now I am changed")
+    const updatedMenu = [...userMenu];
+    updatedMenu[index] = { ...updatedMenu[index], [field]: value };
+    setUserMenu(updatedMenu);
+  };
+
+  const mealList = ["Breakfast", "Lunch", "Dinner"];
 
   return (
     <div className="Table">
@@ -21,13 +52,25 @@ function TableView() {
         </tr>
       </thead>
       <tbody>
-        {userMenu.map((val) => {
+        {userMenu.map((val, index) => {
               return (
                   <tr key={val.Day}>
                       <td>{val.Day}</td>
-                      <td>{val.Breakfast}</td>
-                      <td>{val.Lunch}</td>
-                      <td>{val.Dinner}</td>
+                      {
+                        mealList.map((meal) => {
+                              return (
+                                  <td>
+                                    <SingleMeal 
+                                      row={val} 
+                                      mealTime={meal}
+                                      handleChange={handleChange}
+                                      index={index}
+                                      isFocused={mealSelectedArray[index][meal]}
+                                      toggleMealSelected={toggleMealSelected}
+                                    />
+                                  </td>
+                              )})
+                      }
                   </tr>
               )
           })}
@@ -42,6 +85,32 @@ function TableView() {
 }
 export default TableView;
 
-function singleMeal({row, mealTime}) {
+function SingleMeal({
+  row, mealTime, handleChange, index, isFocused, toggleMealSelected
+}) {
+  // const [isFocused, setIsFocused] = useState(false);
+
+  if (!isFocused) {
+    return (
+      <input
+        className="meal-input"
+        type="text"
+        value={row[mealTime]}
+        // onChange={(e) => handleChange(index, mealTime, e.target.value)}
+        onSelect={() => toggleMealSelected(index, mealTime, true)}
+      />
+    );
+  } else {
+    console.log("Now a text area");
+    console.log(row[mealTime]);
+    return (
+      <textarea
+      className="meal-input-focus"
+      value={row[mealTime]}
+      onChange={(e) => handleChange(index, mealTime, e.target.value)}
+      onBlur={() => toggleMealSelected(index, mealTime, false)}
+    />
+    )
+  }
 
 }
