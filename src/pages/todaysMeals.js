5dbfcf4a91e78cd27cd4c5225b732e758e40cf4e
Prakhar Jain <prakhar.jain@qure.ai>
2024-06-06 08:48:32 +0530

    added todays meal view page

--- src/pages/todaysMeals.js
+++ src/pages/todaysMeals.js
@@ -1,6 +1,11 @@
 import React, { useState } from 'react';
 import '../MealView.css';
 import Modal from '../Modal.js';
+import breakfast_img from '../data/mealView/Breakfast.webp';
+import lunch_img from '../data/mealView/Lunch.webp';
+import dinner_img from '../data/mealView/Dinner.webp';
+import weeklyMenu from "../WeeklyMenu";
+
 
 const MealView = () => {
   const [selectedMeal, setSelectedMeal] = useState(null);
@@ -14,11 +19,15 @@ const MealView = () => {
     year: 'numeric',
   });
 
-  const meals = {
-    breakfast: ['A', 'B', 'C'],
-    lunch: ['A', 'B', 'C'],
-    dinner: ['A', 'B', 'C']
-  };
+  const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+  const dayName = days[currentDate.getDay()];
+
+  const meals = weeklyMenu[dayName];
+  const background_image = {
+    Breakfast: breakfast_img,
+    Lunch: lunch_img,
+    Dinner: dinner_img,
+  }
 
   const handleMealClick = (meal) => {
     setSelectedMeal(meal);
@@ -29,12 +38,21 @@ const MealView = () => {
     <div className="meal-view">
       <div className="header">
         <h1>Good Morning, Unknown</h1>
-        <p>{formattedDate}</p>
+        <h1>{formattedDate}</h1>
       </div>
       <div className="meal-tabs">
         {Object.keys(meals).map((meal) => (
-          <div key={meal} className="meal-tab" onClick={() => handleMealClick(meal)}>
-            <h2>{meal.charAt(0).toUpperCase() + meal.slice(1)}</h2>
+          <div key={meal}
+            className="meal-tab" 
+            onClick={() => handleMealClick(meal)}
+          >
+            <div 
+              className="image-holder"
+              style={{
+                backgroundImage: `url(${background_image[meal]})`
+              }}
+            ></div>
+            <div className="meal-name">{meal.toUpperCase()}</div>
             <div className="food-items">{meals[meal].join(', ')}</div>
           </div>
         ))}
